9ec1bd75b4cfaa37da400c4981f97501a54e8f40
FrankLin <linliawxm@gmail.com>
2020-11-19 23:01:04 -0500

    1. ADD 3 times retry if downloading or uploading was failed 2. Change debug.log file recording level to only ERROR 3. Change path of log.txt and debug.log to same location with this script

--- TosOptionChainAcq.py
+++ TosOptionChainAcq.py
@@ -15,7 +15,7 @@ import logging
 #     f.write(itemName)
 
 #Record debug info for diagnostic
-logging.basicConfig(filename=r'c:\debug.log', format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename=r'debug.log', format='%(asctime)s %(message)s', level=logging.ERROR)
 #Record all detail activities
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 recording = [now]
@@ -42,99 +42,113 @@ def UploadToSqlThread():
         cursor = conn.cursor()
         cursor.fast_executemany = True
         num_sym_up = 0
-        while True:
+        upload_done = False
+        while upload_done == False:
             #Read data from queue
             data = data_q.get()
-            if num_sym_up == 0:
-                up_start_time = time.time()
-            UploadDateTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            failedSym=[]
-            #Upload data for OptionChainHeader
-            try:
-                headerList = [data['symbol'],data['Trading_Day'],data['Trading_DateTime'],data['underlyingPrice'],UploadDateTime ]
+            isSuccess = False
+            tryCnt = 0
+            while tryCnt < 3 and isSuccess == False:
+                tryCnt = tryCnt + 1
+                if num_sym_up == 0:
+                    up_start_time = time.time()
+                UploadDateTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                failedSym=[]
+                #Upload data for OptionChainHeader
+                try:
+                    headerList = [data['symbol'],data['Trading_Day'],data['Trading_DateTime'],data['underlyingPrice'],UploadDateTime ]
 
-                var_string = ', '.join('?' * len(headerList))
-                query_string = 'INSERT INTO OptionChainHeader VALUES (%s);' % var_string
-                cursor.execute(query_string, headerList)
-            except pyodbc.DataError as errd:
-                conn.rollback()    # probably unnecessary
-                header_err = '{0}:{1} header record insert failed: {2}'.format(UploadDateTime, data['symbol'], errd)
-                logging.error(header_err)
-                #print(header_err)
-                failedSym.append(data['symbol'])
-            else:
-                detail_fail = True
-                headerRecId = cursor.execute('SELECT @@IDENTITY AS id;').fetchone()[0]
-                if headerRecId:
-                    #conn.commit()  #Commit until details finished
-                    #Insert PUT contract
-                    cont_num = 0
-                    all_rows = []
-                    for contDays in data['putExpDateMap'].values():
-                        for contPrices in contDays.values():
-                            #delete unused data
-                            del contPrices[0]['bidAskSize']
-                            del contPrices[0]['tradeTimeInLong'] 
-                            del contPrices[0]['quoteTimeInLong'] 
-                            del contPrices[0]['optionDeliverablesList']
-                            del contPrices[0]['isIndexOption']
-                            #update date format from seconds to date
-                            l_time = contPrices[0]['expirationDate']/1000
-                            contPrices[0]['expirationDate'] = dt.fromtimestamp(l_time).strftime("%Y-%m-%d")
-                            l_time = contPrices[0]['lastTradingDay']/1000
-                            contPrices[0]['lastTradingDay'] = dt.fromtimestamp(l_time).strftime("%Y-%m-%d")
-                            #contPrices[0]['HeaderRecID'] = int(headerRecId)
-                            #Update Boolean True&False to 1&0
-                            contPrices[0]['inTheMoney'] = int(contPrices[0]['inTheMoney'])
-                            contPrices[0]['mini'] = int(contPrices[0]['mini'])
-                            contPrices[0]['nonStandard'] = int(contPrices[0]['nonStandard'])
-                            contPrices[0]['tradeDate'] = data['Trading_Day']
+                    var_string = ', '.join('?' * len(headerList))
+                    query_string = 'INSERT INTO OptionChainHeader VALUES (%s);' % var_string
+                    cursor.execute(query_string, headerList)
+                except pyodbc.DataError as errd:
+                    conn.rollback()    # probably unnecessary
+                    header_err = '{0}:{1} header record insert failed: {2}'.format(UploadDateTime, data['symbol'], errd)
+                    logging.error(header_err)
+                    #print(header_err)
+                    #failedSym.append(data['symbol'])
+                else:
+                    detail_fail = True
+                    headerRecId = cursor.execute('SELECT @@IDENTITY AS id;').fetchone()[0]
+                    if headerRecId:
+                        #conn.commit()  #Commit until details finished
+                        #Insert PUT contract
+                        cont_num = 0
+                        all_rows = []
+                        for contDays in data['putExpDateMap'].values():
+                            for contPrices in contDays.values():
+                                #delete unused data
+                                del contPrices[0]['bidAskSize']
+                                del contPrices[0]['tradeTimeInLong'] 
+                                del contPrices[0]['quoteTimeInLong'] 
+                                del contPrices[0]['optionDeliverablesList']
+                                del contPrices[0]['isIndexOption']
+                                #update date format from seconds to date
+                                l_time = contPrices[0]['expirationDate']/1000
+                                contPrices[0]['expirationDate'] = dt.fromtimestamp(l_time).strftime("%Y-%m-%d")
+                                l_time = contPrices[0]['lastTradingDay']/1000
+                                contPrices[0]['lastTradingDay'] = dt.fromtimestamp(l_time).strftime("%Y-%m-%d")
+                                #contPrices[0]['HeaderRecID'] = int(headerRecId)
+                                #Update Boolean True&False to 1&0
+                                contPrices[0]['inTheMoney'] = int(contPrices[0]['inTheMoney'])
+                                contPrices[0]['mini'] = int(contPrices[0]['mini'])
+                                contPrices[0]['nonStandard'] = int(contPrices[0]['nonStandard'])
+                                contPrices[0]['tradeDate'] = data['Trading_Day']
 
-                            if contPrices[0]['putCall'] == 'PUT':
-                                contPrices[0]['putCall'] = 'P'
+                                if contPrices[0]['putCall'] == 'PUT':
+                                    contPrices[0]['putCall'] = 'P'
+                                else:
+                                    contPrices[0]['putCall'] = 'C'
+
+                                values = list(contPrices[0].values())
+                                values.insert(0,int(headerRecId))
+                                if 'NaN' not in values:
+                                    #placeholders = ', '.join(['?']* len(values)) 
+                                    #dict_values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in values)
+                                    all_rows.append(values)
+                                    
+                                    cont_num = cont_num + 1
+                                    #print('{0}:{1} Contract:{2} uploaded'.format(num_sym_up,data['symbol'],cont_num))
+                        var_string = ', '.join('?' * len(values))
+                        query_string = "INSERT INTO OptionChainDetails VALUES ( %s );" % (var_string)
+                        if all_rows:
+                            try:
+                                cursor.executemany(query_string,all_rows)
+                            except:
+                                exe_many_err = '{0} details executemany failed'.format(data['symbol'])
+                                logging.error(exe_many_err)
+                                #print(exe_many_err)
+                                #failedSym.append(data['symbol'])
                             else:
-                                contPrices[0]['putCall'] = 'C'
-
-                            values = list(contPrices[0].values())
-                            values.insert(0,int(headerRecId))
-                            if 'NaN' not in values:
-                                #placeholders = ', '.join(['?']* len(values)) 
-                                #dict_values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in values)
-                                all_rows.append(values)
-                                
-                                cont_num = cont_num + 1
-                                #print('{0}:{1} Contract:{2} uploaded'.format(num_sym_up,data['symbol'],cont_num))
-                    var_string = ', '.join('?' * len(values))
-                    query_string = "INSERT INTO OptionChainDetails VALUES ( %s );" % (var_string)
-                    if all_rows:
-                        try:
-                            cursor.executemany(query_string,all_rows)
-                        except:
-                            exe_many_err = '{0} details executemany failed'.format(data['symbol'])
-                            logging.error(exe_many_err)
-                            #print(exe_many_err)
-                            failedSym.append(data['symbol'])
+                                detail_fail = False
                         else:
-                            detail_fail = False
+                            conn.rollback()
+                            empty_err = '{0} details uploading failed: Empty record'.format(data['symbol'])
+                            logging.error(empty_err)
+                            #print(empty_err)
+                            #failedSym.append(data['symbol'])
                     else:
                         conn.rollback()
-                        empty_err = '{0} details uploading failed: Empty record'.format(data['symbol'])
-                        logging.error(empty_err)
-                        #print(empty_err)
-                        failedSym.append(data['symbol'])
-                else:
-                    conn.rollback()
-                    recId_err = '{0} header RecId retrieve failed'.format(data['symbol'])
-                    logging.error(recId_err)
-                    #print(recId_err)
-                    failedSym.append(data['symbol'])
-                if not detail_fail:
-                    conn.commit()
-                    num_sym_up = num_sym_up + 1
-                    print('{0}:{1} uploaded'.format(num_sym_up,data['symbol']))
-                    
-                    if download_done == True and num_sym_up == down_num_sym:
-                        break
+                        recId_err = '{0} header RecId retrieve failed'.format(data['symbol'])
+                        logging.error(recId_err)
+                        #print(recId_err)
+                        #failedSym.append(data['symbol'])
+                    if not detail_fail:
+                        conn.commit()
+                        isSuccess = True
+                        num_sym_up = num_sym_up + 1
+                        print('{0}:{1} uploaded'.format(num_sym_up,data['symbol']))
+                        if download_done == True and num_sym_up == down_num_sym:
+                             upload_done = True
+                if isSuccess == False:
+                    #if failed, maybe caused by connection error, reconnect it
+                    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+                    cursor = conn.cursor()
+                    cursor.fast_executemany = True
+            if isSuccess == False:
+                #3 times trial still failed, log the symbol
+                failedSym.append(data['symbol'])   
+                           
         conn.close()
     except pyodbc.Error as err:
         logging.error(err)
@@ -158,45 +172,52 @@ down_num_sym = 0
 down_fail_num = 0
 #Downloading process
 for symbol in stocksymbols:
-    time.sleep(0.6)
-    downFailSym = []
-    #define our payload
-    payload = {'apikey':clientId,
-                'symbol':symbol}
-    try:
-        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        #make a request
-        content = requests.get(url=endpoint,params=payload)
-        content.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        logging.error('Http error - ' + str(errh))
-        downFailSym.append(symbol)
-    except requests.exceions.ConnectionError as errc:
-        logging.error('Error Connecptting:' + str(errc))
-        downFailSym.append(symbol)
-    except requests.exceptions.Timeout as errt:
-        logging.error('Timeout Error:' + str(errt))
-        downFailSym.append(symbol)
-    except requests.exceptions.RequestException as err:
-        logging.error('Download request failed:' + str(err))
-        downFailSym.append(symbol)
-    else:#Downloading successfully
-        #convert it to disctionary
-        data = content.json()
-        #Add addtional two column data
-        data['Trading_DateTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        data['Trading_Day'] = time.strftime("%Y-%m-%d", time.localtime())
-        #Push the data to queue which will be processed by Uploading thread
-        data_q.put(data)
+    isSuccess = False
+    tryCnt = 0
+    while tryCnt < 3 and isSuccess == False:
+        tryCnt = tryCnt + 1
+        #time.sleep(0.6)
+        downFailSym = []
+        #define our payload
+        payload = {'apikey':clientId,
+                    'symbol':symbol}
+        try:
+            now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            #make a request
+            content = requests.get(url=endpoint,params=payload)
+            content.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            logging.error('Http error - ' + str(errh))
+            #downFailSym.append(symbol)
+        except requests.exceions.ConnectionError as errc:
+            logging.error('Error Connecptting:' + str(errc))
+            #downFailSym.append(symbol)
+        except requests.exceptions.Timeout as errt:
+            logging.error('Timeout Error:' + str(errt))
+            #downFailSym.append(symbol)
+        except requests.exceptions.RequestException as err:
+            logging.error('Download request failed:' + str(err))
+            #downFailSym.append(symbol)
+        else:#Downloading successfully
+            isSuccess = True
+            #convert it to disctionary
+            data = content.json()
+            #Add addtional two column data
+            data['Trading_DateTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            data['Trading_Day'] = time.strftime("%Y-%m-%d", time.localtime())
+            #Push the data to queue which will be processed by Uploading thread
+            data_q.put(data)
 
-        down_num_sym = down_num_sym + 1
-        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            down_num_sym = down_num_sym + 1
+            now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        print('{0}:{1} downloaded'.format(down_num_sym,symbol))
-        #Save data to file
-        #filename = symbol + '.json'
-        #with open(filename, 'w+') as f:
-        #    json.dump(data,f,indent=3)
+            print('{0}:{1} downloaded'.format(down_num_sym,symbol))
+            #Save data to file
+            #filename = symbol + '.json'
+            #with open(filename, 'w+') as f:
+            #    json.dump(data,f,indent=3)
+    if isSuccess == False:
+        downFailSym.append(symbol)
 download_done = True
 end_time = time.time()
 detal = int(end_time - start_time)
@@ -209,7 +230,7 @@ recording.append(downFailSym)
 th.join()
 
 recording.append(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-with open('C:\log.txt','a') as f:
+with open('log.txt','a') as f:
     recordValue = " ".join([str(elem) for elem in recording])
     f.write(recordValue)
     f.write('\n')
